b5d49d7b7d8cbae47c0760dc4aabe24f2822ff93
Allwyn Mendes <allwyn.mendes@coniq.com>
2018-07-11 15:16:23 +0100

    Playing around with Doctrine Part 1

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Prouct;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -14,8 +15,68 @@ class DefaultController extends Controller
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
+        echo 'Text';
+        //$this->createAction('USB Drive', 15, '1PB USB 12.1 Drive');
+        //$this->showAction2(1);
+        $this->showAction3(15);
+
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
     }
+
+    public function createAction($name, $price, $desc)
+    {
+        echo '/nInside createAction()';
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $product = new Prouct();
+        $product->setName($name);
+        $product->setPrice($price);
+        $product->setDescription($desc);
+
+        $entityManager->persist($product);
+        $entityManager->flush();
+
+        //return new Response('Saved new product with id '.$product->getId());
+    }
+
+    public function showAction($productId)
+    {
+        $product = $this->getDoctrine()->getRepository(Product::class)->find($productId);
+
+        if(!$product){
+            throw $this->createNotFoundException('No product found with id '.$productId);
+        }
+
+    }
+
+    public function showAction2($productId)
+    {
+        echo '\nInside showAction2()';
+        $repository = $this->getDoctrine()->getRepository(Prouct::class);
+        $product = $repository->find($productId);
+
+        if(!$product){
+            echo 'No data';
+        }else{
+            echo '/nName : ' . $product->getName();
+        }
+    }
+
+    public function showAction3($price){
+        echo '\nInside showAction3()';
+        $repository = $this->getDoctrine()->getRepository(Prouct::class);
+        $product = $repository->findByPrice($price);
+
+        if(!$product){
+            echo 'No data';
+        }else {
+            foreach ($product as $prod){
+                echo '/nName : ' . $prod->getName();
+            }
+        }
+    }
+
+
 }
