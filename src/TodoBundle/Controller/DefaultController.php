677dda89ad7e69a3778e42a526044c41ca3ffda9
Allwyn Mendes <allwyn.mendes@coniq.com>
2018-07-24 17:04:50 +0100

    Created a basic form in Controller

--- src/TodoBundle/Controller/DefaultController.php
+++ src/TodoBundle/Controller/DefaultController.php
@@ -2,16 +2,43 @@
 
 namespace TodoBundle\Controller;
 
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
+use TodoBundle\Entity\TodoList;
 
 class DefaultController extends Controller
 {
     /**
      * @Route("/todolist")
      */
-    public function indexAction()
+    public function indexAction(Request $request)
     {
-        return $this->render('@TodoBundle/Resources/views/todohome.html.twig');
+        $todoObj = new TodoList();
+
+        $todoObj->setTodo('Kill That Task');
+
+        $form = $this->createFormBuilder($todoObj)
+            ->add('Todo', TextType::class)
+            ->add('Add', SubmitType::class)
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isValid()){
+            echo 'Form is Valid';
+            echo 'Todo : '.$todoObj->getTodo();
+        }else{
+            echo 'Form is In Valid';
+        }
+
+
+
+        return $this->render('@TodoBundle/Resources/views/todohome.html.twig', array(
+            'ourForm' => $form->createView()
+        ));
     }
 }
